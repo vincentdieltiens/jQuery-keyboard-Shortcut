613468fff5dfd5bb9a8eff4a997bac91e3523535
Vincent Dieltiens <and1hotsauce@gmail.com>
2011-11-06 11:07:25 +0100

    lets support more shortcuts like 'alt+right', 'cmd+up', etc.

--- jquery.keyboardShortcut.js
+++ jquery.keyboardShortcut.js
@@ -47,6 +47,91 @@
         this.bindings = $.extend({}, this.options.bindings);
         
         this._init_handlers();
+        
+        this.keyStrings = {
+            3: 'cancel',
+            6: 'help',
+            8: 'backspace',
+            9: 'tab',
+            12: 'clear',
+            13: 'return',
+            14: 'enter',
+            16: 'shift',
+            17: 'ctrl',
+            18: 'alt',
+            19: 'pause',
+            20: 'capslock',
+            27: 'escape',
+            32: 'space',
+            33: 'pageup',
+            34: 'pagedown',
+            35: 'end',
+            36: 'home',
+            37: 'left',
+            38: 'up',
+            39: 'right',
+            40: 'down',
+            44: 'printscreen',
+            45: 'insert',
+            46: 'del',
+            48: '0',
+            49: '1',
+            50: '2',
+            51: '4',
+            52: '4',
+            53: '5',
+            54: '6',
+            55: '7',
+            56: '8',
+            57: '9',
+            59: ';',
+            65: 'a',
+            66: 'b',
+            67: 'c',
+            68: 'd',
+            69: 'e',
+            70: 'f',
+            71: 'g',
+            72: 'h',
+            73: 'i',
+            74: 'j',
+            75: 'k',
+            76: 'l',
+            77: 'm',
+            78: 'n',
+            79: 'o',
+            80: 'p',
+            81: 'q',
+            82: 'r',
+            83: 's',
+            84: 't',
+            85: 'u',
+            86: 'v',
+            87: 'w',
+            88: 'x',
+            89: 'y',
+            90: 'z',
+            96: 'pad0',
+            97: 'pad1',
+            98: 'pad2',
+            99: 'pad3',
+            100: 'pad4',
+            101: 'pad5',
+            102: 'pad6',
+            103: 'pad7',
+            104: 'pad8',
+            105: 'pad9',
+            106: '*',
+            107: 'plus',
+            109: 'sub',
+            111: '/',
+            188: ',',
+            190: '.',
+            191: '/',
+            192: '`',
+            219: '(',
+            221: ')' 
+        };
     };
     
     KeyboardShortcut.prototype = {
@@ -62,7 +147,13 @@
                     return;
                 }
                 
-                var cmd = String.fromCharCode(e.keyCode).toLowerCase();
+                if( !self.has_key_string(e.keyCode) ) {
+                    return;
+                }
+                
+                
+                console.log(e.keyCode+ '-'+ self.get_key_string(e.keyCode));
+                var cmd = self.get_key_string(e.keyCode);
                 
                 if( e.shiftKey == true ) {
                     cmd = "shift+"+cmd;
@@ -90,6 +181,15 @@
                     action();
                 }
             });
+        },
+        get_key_string: function(key) {
+            if( key in this.keyStrings ) {
+                return this.keyStrings[key];
+            }
+            return null;
+        },
+        has_key_string: function(key) {
+            return key in this.keyStrings;
         },
         bind: function(cmd, f, auth) {
             if( typeof auth == 'undefined' ) {
